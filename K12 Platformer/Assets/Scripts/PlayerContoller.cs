902b5d7ef3a0646e58739e18483b591cdc5df193
TageOlson <71279866+TageOlson@users.noreply.github.com>
2021-06-11 12:51:09 -0700

    Smoother Player Movement
    Less control in the air for the player. Side inputs effect velocity less in the air and player slows horizontal movement when airborne.
    Jump height is now affected by how long the jump button is pressed.
    Jump falls faster to feel less floaty.
    
    Test level has also been edited to test movements and kill floor can no longer be hit.

--- K12 Platformer/Assets/Scripts/PlayerContoller.cs
+++ K12 Platformer/Assets/Scripts/PlayerContoller.cs
@@ -8,7 +8,12 @@ public class PlayerContoller : MonoBehaviour
     Rigidbody2D myRigidbody;
     LifeKeeper theLifeKeeper;
     [SerializeField] float runSpeed = 5f;
-    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float jumpSpeed = 8f;
+    [SerializeField] float fallSpeedMultiplier = 1.5f;
+    [SerializeField] float jumpSpeedMultiplier = 1.25f;
+    [Range(0, 0.05f)] [SerializeField] float jumpDamping = 0.005f;
+    bool isGrounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,31 +25,57 @@ public class PlayerContoller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GroundCheck();
         Run();
         Jump();
+        SmoothJumping();
     }
     private void Run()
     {
         float controlDirection = Input.GetAxis("Horizontal");
-        Vector2 playerVelocity = new Vector2(controlDirection * runSpeed,
-            myRigidbody.velocity.y);
-            myRigidbody.velocity = playerVelocity;
+        Vector2 playerVelocity = new Vector2(myRigidbody.velocity.x, myRigidbody.velocity.y);
+        if(!isGrounded)
+        { //less movement in air
+            playerVelocity.x += (controlDirection * runSpeed * .3f *jumpDamping);
+            //Debug.Log(playerVelocity.x);
+            playerVelocity.x = Mathf.Lerp(playerVelocity.x, 0f, .5f * jumpDamping);//prevents horizontal movement from going too far in the air
+        }
+        else
+        {
+            playerVelocity.x = controlDirection * runSpeed;
+        }
+        playerVelocity.x = Mathf.Clamp(playerVelocity.x, -runSpeed, runSpeed);
+        myRigidbody.velocity = playerVelocity;
     }
     private void Jump()
     {
-        if(!myCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        if(!isGrounded)
         {
             return;
         }
         if(Input.GetButtonDown("Jump")) // spacebar
         {
-            Vector2 jumpVelocityToAdd = new Vector2(0f,jumpSpeed);
-            myRigidbody.velocity += jumpVelocityToAdd;
+            myRigidbody.velocity = Vector2.up * jumpSpeed;
+        }
+    }
+    private void SmoothJumping()
+    {
+        if(myRigidbody.velocity.y < 0)
+        {//faster falling
+            myRigidbody.velocity += Vector2.up * Physics2D.gravity.y * fallSpeedMultiplier * Time.deltaTime;
+        }
+        else if(myRigidbody.velocity.y > 0 && !Input.GetButton("Jump"))
+        {//moving up and not holding jump, short hop
+            myRigidbody.velocity += Vector2.up * Physics2D.gravity.y * jumpSpeedMultiplier * Time.deltaTime;
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         theLifeKeeper.DecreaseLives();
         Destroy(gameObject);
+    }
+    private void GroundCheck()
+    {
+        isGrounded = myCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
     }
 }
